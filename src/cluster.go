eea23975bc9aa23dcd87403233fc47dc58679903
Drew Russell <Drew.Russell@rubrik.com>
2018-10-07 10:55:03 -0500

    add ClusterNodeIP method

--- src/cluster.go
+++ src/cluster.go
@@ -1,7 +1,20 @@
 package rubrikcdm
 
+// ClusterVersion returns the CDM version of the Rubrik cluster
 func (c *Credentials) ClusterVersion() interface{} {
-	cluster := c.Get("v1", "/cluster/me")
-	return cluster["version"]
+	apiRequest := c.Get("v1", "/cluster/me")
+	return apiRequest["version"]
+}
 
+// ClusterNodeIP returns a slice of all Node IPs in the Rubrik cluster
+func (c *Credentials) ClusterNodeIP() []string {
+	apiRequest := c.Get("internal", "/cluster/me/node")
+
+	var nodeList []string
+
+	for _, v := range apiRequest["data"].([]interface{}) {
+		nodeList = append(nodeList, v.(interface{}).(map[string]interface{})["ipAddress"].(string))
+	}
+
+	return nodeList
 }
