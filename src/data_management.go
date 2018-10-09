d420143013063c2e1747e0256951145d6d8cb316
Drew Russell <Drew.Russell@rubrik.com>
2018-10-08 20:57:24 -0500

    support for physicalHost, filesetTemplate and managedVolume to ObjectID

--- src/data_management.go
+++ src/data_management.go
@@ -6,16 +6,19 @@ import (
 )
 
 // ObjectID will search the Rubrik cluster for the provided objectName and return its Id
-func (c *Credentials) ObjectID(objectType, objectName string) string {
+func (c *Credentials) ObjectID(objectType, objectName string, hostOS ...string) string {
 
 	validObjectType := map[string]bool{
-		"vmware":     true,
-		"sla":        true,
-		"vmwareHost": true,
+		"vmware":          true,
+		"sla":             true,
+		"vmwareHost":      true,
+		"physicalHost":    true,
+		"filesetTemplate": true,
+		"managedVolume":   true,
 	}
 
 	if validObjectType[objectType] == false {
-		log.Fatalf("Error: The `objectType` must be `vmware`, `sla`, or `vmwareHost`.")
+		log.Fatalf("Error: The `objectType` must be `vmware`, `sla`, `vmwareHost`, `physicalHost`, `filesetTemplate`, or `managedVolume`.")
 	}
 	var objectSummaryAPIVersion string
 	var objectSummaryAPIEndpoint string
@@ -30,18 +33,54 @@ func (c *Credentials) ObjectID(objectType, objectName string) string {
 	case "vmwareHost":
 		objectSummaryAPIVersion = "v1"
 		objectSummaryAPIEndpoint = "/vmware/host?primary_cluster_id=local"
+	case "filesetTemplate":
+		var hostOperatingSystem string
+		if len(hostOS) > 0 {
+			hostOperatingSystem = hostOS[0]
+			switch hostOperatingSystem {
+			case "Linux":
+			case "Windows":
+			default:
+				log.Fatalf("Error: The hostOS must be either 'Linux' or 'Windows'.")
+
+			}
+		} else if len(hostOS) == 0 {
+			log.Fatalf("Error: You must provide the Fileset Tempalte OS type. ")
+
+		}
+
+		objectSummaryAPIVersion = "v1"
+		objectSummaryAPIEndpoint = fmt.Sprintf("/fileset_template?primary_cluster_id=local&operating_system_type=%s&name=%s", hostOperatingSystem, objectName)
+	case "managedVolume":
+		objectSummaryAPIVersion = "internal"
+		objectSummaryAPIEndpoint = fmt.Sprintf("/managed_volume?is_relic=false&primary_cluster_id=local&name=%s", objectName)
 	}
 
 	apiRequest := c.Get(objectSummaryAPIVersion, objectSummaryAPIEndpoint)
-
 	if apiRequest["total"] == 0 {
 		log.Fatalf(fmt.Sprintf("Error: The %s object '%s' was not found on the Rubrik cluster.", objectType, objectName))
-	} else {
+	} else if apiRequest["total"].(float64) > 0 {
+		objectIDs := make([]string, 0)
+		// # Define the "object name" to search for
+		var nameValue string
+		if objectType == "physicalHost" {
+			nameValue = "hostname"
+		} else {
+			nameValue = "name"
+		}
 
 		for _, v := range apiRequest["data"].([]interface{}) {
-			if v.(interface{}).(map[string]interface{})["name"].(string) == objectName {
-				return v.(interface{}).(map[string]interface{})["id"].(string)
+			if v.(interface{}).(map[string]interface{})[nameValue].(string) == objectName {
+				objectIDs = append(objectIDs, v.(interface{}).(map[string]interface{})["id"].(string))
 			}
+		}
+
+		if len(objectIDs) > 1 {
+			log.Fatalf(fmt.Sprintf("Error: Multiple %s objects named '%s' were found on the Rubrik cluster. Unable to return a specific object id.", objectType, objectName))
+		} else if len(objectIDs) == 0 {
+			log.Fatalf(fmt.Sprintf("Error: The %s object '%s' was not found on the Rubrik cluster.", objectType, objectName))
+		} else {
+			return objectIDs[0]
 		}
 	}
 
