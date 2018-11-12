98709e6ad55c387a566d1461ab148e8d6a658475
Drew Russell <Drew.Russell@rubrik.com>
2018-11-12 00:31:59 -0700

    add copyright info

--- rubrikcdm/client.go
+++ rubrikcdm/client.go
@@ -1,3 +1,16 @@
+// Copyright 2018 Rubrik, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License prop
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package rubrikcdm transforms the Rubrik API functionality into easy to consume functions. This eliminates the need to understand
 // how to consume raw Rubrik APIs with Go and extends upon one of Rubrik’s main design centers - simplicity. Rubrik’s API first architecture enables
 // organizations to embrace and integrate Rubrik functionality into their existing automation processes.
